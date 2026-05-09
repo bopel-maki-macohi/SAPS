6cbe7ff18e527e6f04c4da676a4687d549c3915f
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-09 17:29:55 -0500

    jumpin (this shit about to look ass)

--- source/levels/LevelOne.hx
+++ source/levels/LevelOne.hx
@@ -1,5 +1,9 @@
 package levels;
 
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.math.FlxMath;
+import flixel.FlxG;
 import flixel.group.FlxSpriteGroup;
 import flixel.util.FlxColor;
 import flixel.FlxSprite;
@@ -7,19 +11,41 @@ import flixel.FlxState;
 
 class LevelOne extends FlxState
 {
-    public var sinco:FlxSprite;
+	public var sinco:FlxSprite;
 
-    public var obstacles:FlxSpriteGroup;
-    
-    override function create() {
-        super.create();
+	public var obstacles:FlxSpriteGroup;
 
-        sinco = new FlxSprite().makeGraphic(128, 128, FlxColor.LIME);
-        add(sinco);
+	override function create()
+	{
+		super.create();
 
-        sinco.screenCenter();
+		sinco = new FlxSprite().makeGraphic(128, 128, FlxColor.LIME);
+		add(sinco);
 
-        obstacles = new FlxSpriteGroup();
-        add(obstacles);
-    }
+		sinco.screenCenter();
+
+		obstacles = new FlxSpriteGroup();
+		add(obstacles);
+	}
+
+	public var jumping:Bool = false;
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+		if (FlxG.keys.justPressed.SPACE && !jumping)
+		{
+			jumping = true;
+
+			FlxTween.tween(sinco, {y: sinco.y - sinco.height * 4}, .25, {ease: FlxEase.backOut})
+				.then(FlxTween.tween(sinco, {y: sinco.y + sinco.height * 4}, .25, {
+					ease: FlxEase.backIn,
+					onComplete: t ->
+					{
+						jumping = false;
+					}
+				}));
+		}
+	}
 }
