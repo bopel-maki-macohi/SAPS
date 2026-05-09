8661c8594a8eb14c764cadb0de079ec6868268f6
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-09 17:22:33 -0500

    bullshit title state

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -1,12 +1,15 @@
 package;
 
+import flixel.text.FlxText;
 import flixel.FlxState;
 
-class PlayState extends FlxState
+class TitleState extends FlxState
 {
 	override public function create()
 	{
 		super.create();
+
+		add(new FlxText(0,0,0,'Press ENTER', 16));
 	}
 
 	override public function update(elapsed:Float)
