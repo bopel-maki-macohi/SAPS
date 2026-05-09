b05533fed5bd966471da4642aad9cc1db9735faa
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-09 17:24:46 -0500

    dummy sinco, assets l8r

--- source/levels/LevelOne.hx
+++ source/levels/LevelOne.hx
@@ -1,5 +1,19 @@
 package levels;
 
+import flixel.util.FlxColor;
+import flixel.FlxSprite;
 import flixel.FlxState;
 
-class LevelOne extends FlxState {}
+class LevelOne extends FlxState
+{
+    public var sinco:FlxSprite;
+    
+    override function create() {
+        super.create();
+
+        sinco = new FlxSprite().makeGraphic(128, 128, FlxColor.LIME);
+        add(sinco);
+
+        sinco.screenCenter();
+    }
+}
